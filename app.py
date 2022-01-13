31b2a0d2a908111a261092288eded3e40634aaaf
Gwidas <gwidas.hiroshi@icloud.com>
2022-01-13 17:07:44 +0000

    fixed home page link

--- app.py
+++ app.py
@@ -18,14 +18,40 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+def root():
+    return render_template("home.html")
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
 
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 

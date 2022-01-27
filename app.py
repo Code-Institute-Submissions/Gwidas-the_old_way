12b56d405dc6806d63bad80ce3822001c266da6e
Gwidas <gwidas.hiroshi@icloud.com>
2022-01-27 18:01:12 +0000

    chenged debug mode to false

--- app.py
+++ app.py
@@ -164,5 +164,5 @@ def logout():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=True
+        host=os.environ.get("IP"), port=int(os.environ.get("PORT")), debug=False
     )  
